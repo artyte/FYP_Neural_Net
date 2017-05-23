c3bf0570b013dfba0baf4da85c7fddc4805d1b8f
artyte <artyte@gmail.com>
2017-05-24 04:57:51 +0800

    tested basic tree traversal, still working on getting correct sentence

--- grammar_encoder_decoder/preprocess.py
+++ grammar_encoder_decoder/preprocess.py
@@ -1,8 +1,25 @@
 import sys
 import codecs
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 from bs4 import BeautifulSoup as bs
-soup = bs(open("nucle3.2.sgml"), "lxml")
+import pickle
+f = open('nucle3.2.p', 'r')
+soup = pickle.load(f)
 
-print soup.doc
+paragraphs = []
+for p in soup.textword.findChildren('p'): paragraphs += p
+mistakelis = []
+for mistake in soup.annotation.findChildren('mistake'):
+	mistakelis.append([mistake.correction.text, int(mistake['start_par']) - 1, int(mistake['start_off']), int(mistake['end_off'])])
+
+for p in paragraphs: print p
+
+for mistake in mistakelis:
+	para = paragraphs[mistake[1]]
+	para = para[:mistake[2]] + mistake[0] + para[mistake[3]:]
+	paragraphs[mistake[1]] = para
+
+for p in paragraphs: print p
+f.close()
