3644812df0b049b75648f072449c73e3e8349497
artyte <artyte@gmail.com>
2017-06-05 19:53:50 +0800

    stopped using recurrentshop

--- grammar_encoder_decoder/encoder_decoder.py
+++ grammar_encoder_decoder/encoder_decoder.py
@@ -3,13 +3,13 @@ import codecs
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-output_dim = [50, 20, 1]
+output_dim = [200, 20, 10, 1]
 repeat = 200
 max_len = 200
 epochs = 5
 batch = 32
 grad_desc = 'adam'
-error_calc = 'categorical_crossentropy'
+error_calc = 'binary_crossentropy'
 
 def pickle_return(filename):
 	import pickle
@@ -20,34 +20,50 @@ def pickle_return(filename):
 
 def extract_data():
 	from keras.preprocessing.sequence import pad_sequences as ps
+	from keras.utils.np_utils import to_categorical as tc
 	import numpy as np
 	weights = np.load(open("embeds.npy", 'rb'))
 	X_train = ps(pickle_return('training_input_vectors.p'), maxlen=max_len)
 	y_train = ps(pickle_return('training_output_vectors.p'), maxlen=max_len)
 	X_test = ps(pickle_return('testing_input_vectors.p'), maxlen=max_len)
 	y_test = ps(pickle_return('testing_output_vectors.p'), maxlen=max_len)
+	'''
+	X_train = np.asarray(X_train)
+	y_train = np.asarray(y_train)
+	X_test = np.asarray(X_test)
+	y_test = np.asarray(y_test)
+	np.reshape(X_train, (X_train.shape[1], X_train.shape[2], 1))
+	np.reshape(y_train, (y_train.shape[1], y_train.shape[2], 1))
+	np.reshape(X_test, (X_test.shape[1], X_test.shape[2], 1))
+	np.reshape(y_test, (y_test.shape[1], y_test.shape[2], 1))
+	'''
 	index_map = pickle_return('index.p')
 	reverse_index = pickle_return('reverse_index.p')
+	print np.asarray(X_train).shape
 	return weights, X_train, y_train, X_test, y_test, index_map, reverse_index
 
 def train_model(weights, X_train, y_train):
 	from keras.models import Sequential
-	from keras.layers import Embedding, Dense, RepeatVector, TimeDistributed
-	from recurrentshop import RecurrentSequential, LSTMCell, GRUCell
+	from keras.layers import Reshape, Embedding, Dense, RepeatVector,TimeDistributed, GRU, LSTM
+	#from recurrentshop import RecurrentSequential, LSTMCell, GRUCell
 
 	model = Sequential()
 	model.add(Embedding(input_dim=weights.shape[0],
 						output_dim=output_dim[0],
 						weights=[weights],
-						input_length=max_len,
+						input_length=200,
 						trainable=False))
+	'''
 	encoder = RecurrentSequential(state_initializer='random_normal',
 								state_sync=True,
 								teacher_force=True,
 								return_sequences=False)
 	encoder.add(LSTMCell(output_dim[1]))
 	model.add(encoder.get_cell())
+	'''
+	model.add(LSTM(output_dim[1]))
 	model.add(RepeatVector(repeat))
+	'''
 	decoder = RecurrentSequential(state_initializer='random_normal',
 								readout='add',
 								state_sync=True,
@@ -55,8 +71,11 @@ def train_model(weights, X_train, y_train):
 								return_sequences=True)
 	decoder.add(GRUCell(output_dim[2]))
 	model.add(decoder.get_cell())
-	model.add(TimeDistributed(Dense(output_dim[2], activation = 'softmax'))
-	model.compile(optimizer=grad_desc, loss=error_calc, metrics='rmsprop')
+	'''
+	model.add(GRU(output_dim[2], return_sequences=True))
+	print model.summary()
+	model.add(TimeDistributed(Dense(output_dim[3], activation = 'sigmoid')))
+	model.compile(optimizer=grad_desc, loss=error_calc, metrics=['accuracy'])
 	model.fit(X_train, y_train, nb_epoch=epochs, batch_size=batch)
 	return model
 
